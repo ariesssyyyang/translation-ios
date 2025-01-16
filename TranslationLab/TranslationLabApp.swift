5d769b9a61c6c64b75fa8d07b115113d50966f4b
Aries Yang <ariesssyyyang@gmail.com>
2025-01-16 16:04:27 +0800

    [add] new Xcode project

---
+++ TranslationLab/TranslationLabApp.swift
@@ -0,0 +1,32 @@
+//
+//  TranslationLabApp.swift
+//  TranslationLab
+//
+//  Created by Aries on 2025/1/16.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TranslationLabApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
