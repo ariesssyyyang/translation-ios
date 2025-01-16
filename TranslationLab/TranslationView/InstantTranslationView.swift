2028f1392a73c2fccc09bc85bd522625931e1d76
Aries Yang <ariesssyyyang@gmail.com>
2025-01-16 23:18:23 +0800

    [feat] translationTask(source:target:action:)

--- TranslationLab/TranslationView/InstantTranslationView.swift
+++ TranslationLab/TranslationView/InstantTranslationView.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import Translation
 
 struct InstantTranslationView: View {
+
+    private typealias LocaleLanguage = Locale.Language
+
+    @State private var targetText: String?
+
+    @State private var sourceText = """
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    """
+
     var body: some View {
-        Text("Instant translation")
+        Text(targetText ?? sourceText)
+            .padding()
+            .translationTask(source: LocaleLanguage(identifier: "en-US"), target: LocaleLanguage(identifier: "zh-Hant-TW")) { session in
+                do {
+                    let response = try await session.translate(sourceText)
+                    targetText = response.targetText
+                } catch {
+                    print("Error", error)
+                }
+            }
     }
 }
 
